e59b4e8ccd536e0a48db8aa3d60f30a7e7a3738d
Martia <pureromancebymartia@gmail.com>
2022-04-30 22:32:59 -0400

    welcome

--- app.js
+++ app.js
@@ -101,4 +101,131 @@ function mixedNumbers(arr) {
       console.log(quickCheck(['squash', 'onions', 'shallots'], 'mushrooms'));
 
       //#11 Iterate through all an Array's Items using For loops
+      function filteredArray(arr, elem) {
+        let newArr = [];
+        // Only change code below this line
+      for(let i = 0; i < arr.length; i++ ) {
+        if (arr[i].indexOf(elem) == -1) {
+          newArr.push(arr[i]);
+        }
+      }
+        // Only change code above this line
+        return newArr;
+      }
       
+      console.log(filteredArray([[3, 2, 3], [1, 6, 3], [3, 13, 26], [19, 3, 9]], 3));
+      //#12 Create complex multi-dimensional arrays
+      let myNestedArray = [
+        // Only change code below this line
+        ["unshift", false, 1, 2, 3, "complex", "nested"],
+        ["loop", "shift", 6, 7, 1000, "method"],
+        ["concat", false, true, "spread", "array", ["deep"]],
+        ["mutate", 1327.98, "splice", "slice", "push", [["deeper"]]],
+        ["iterate", 1.3849, 7, "8.4876", "arbitrary", "depth", [[["deepest"]]]]
+        // Only change code above this line
+      ];
+
+      //#13 Add key value pairs to Javascript objects
+      let foods = {
+        apples: 25,
+        oranges: 32,
+        plums: 28
+      };
+      
+      // Only change code below this line
+      foods.bananas = 13;
+      foods.grapes = 35;
+      foods.strawberries = 27;
+      // Only change code above this line
+      
+      console.log(foods);
+      
+      //#14 Modify an Object nested within an Object
+      let userActivity = {
+        id: 23894201352,
+        date: 'January 1, 2017',
+        data: {
+          totalUsers: 51,
+          online: 42
+        }
+      };
+      
+      // Only change code below this line
+      userActivity.data.online = 45;
+      // Only change code above this line
+      
+      console.log(userActivity);
+
+      //#15 Access property names with Bracket Notation
+      let foods = {
+        apples: 25,
+        oranges: 32,
+        plums: 28,
+        bananas: 13,
+        grapes: 35,
+        strawberries: 27
+      };
+      
+      function checkInventory(scannedItem) {
+        // Only change code below this line
+      return foods[scannedItem];
+        // Only change code above this line
+      }
+      
+      console.log(checkInventory("apples"));
+
+      //#16 Use the delete keyword to remove object properties
+      let foods = {
+        apples: 25,
+        oranges: 32,
+        plums: 28,
+        bananas: 13,
+        grapes: 35,
+        strawberries: 27
+      };
+      
+      // Only change code below this line
+      delete foods.oranges;
+      delete foods.plums;
+      delete foods.strawberries;
+      // Only change code above this line
+      
+      console.log(foods);
+
+     //#17 Check if an object has a property
+     let users = {
+        Alan: {
+          age: 27,
+          online: true
+        },
+        Jeff: {
+          age: 32,
+          online: true
+        },
+        Sarah: {
+          age: 48,
+          online: true
+        },
+        Ryan: {
+          age: 19,
+          online: true
+        }
+      };
+      
+      function isEveryoneHere(userObj) {
+        // Only change code below this line
+         if (
+          userObj.hasOwnProperty("Alan") &&
+          userObj.hasOwnProperty("Jeff") &&
+          userObj.hasOwnProperty("Sarah") &&
+          userObj.hasOwnProperty("Ryan")
+        ) {
+          return true;
+        }
+        return false;
+        // Only change code above this line
+      }
+      
+      console.log(isEveryoneHere(users));
+
+      //#18 
